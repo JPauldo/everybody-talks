a781e112496f84b235c76d7709d9a27b9f5fd96b
KingdomSeeker328 <30511892+KingdomSeeker328@users.noreply.github.com>
2023-01-14 14:20:59 -0500

    Created the Reaction schema

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,12 +1,57 @@
 const { Schema, Types } = require('mongoose');
 
 const reactionSchema = new Schema(
-  {},
+  {
+    reactionId: {
+      type: Types.ObjectId,
+      default: () => new Types.ObjectId
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      minLength: [1, 'Not enough characters...'],
+      maxLength: [280, 'Character limit of 280 exceed!']
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (date) => {
+        const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+        const nth = (d) => {
+          if (d > 3 && d < 21) {
+            return 'th';
+          } else {
+            switch (d % 10) {
+              case 1:
+                return 'st';
+              case 2:
+                return 'nd';
+              case 3:
+                return 'rd';
+              default:
+                return 'th';
+            }
+          }
+        }
+        
+        const dateNum = date.getDate();
+        const month = months[date.getMonth()];
+        const year = date.getFullYear();
+        const time = date.toLocaleTimeString(undefined, { hour: 'numeric', minute: '2-digit' });
+
+        return `${month} ${nth(dateNum)}, ${year} at ${time}`;
+      }
+    }
+  },
   {
     toJSON: {
       getters: true,
     },
-    id: false,
+    id: false
   }
 );
 
