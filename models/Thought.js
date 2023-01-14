f82b50115ffa3a00bac72363b0810a630afee2ca
KingdomSeeker328 <30511892+KingdomSeeker328@users.noreply.github.com>
2023-01-14 14:14:17 -0500

    Created the Thought schema and model

--- models/Thought.js
+++ models/Thought.js
@@ -3,13 +3,56 @@ const reactionSchema = require('./Reaction');
 
 // Schema to create Student model
 const thoughtSchema = new Schema(
-  {},
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: [1, 'Not enough characters...'],
+      maxLength: [280, 'Character limit of 280 exceed!']
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (date) => {
+        const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sept", "Oct", "Nov", "Dec"];
+        const nth = (d) => {
+          if (d > 3 && d < 21) {
+            return 'th';
+          } else {
+            switch (d % 10) {
+              case 1:
+                return 'st';
+              case 2:
+                return 'nd';
+              case 3:
+                return 'rd';
+              default:
+                return 'th';
+            }
+          }
+        }
+        const dateNum = date.getDate();
+        const month = months[date.getMonth()];
+        const year = date.getFullYear();
+        const time = date.toLocaleTimeString(undefined, { hour: 'numeric', minute: '2-digit' });
+
+        return `${month} ${nth(dateNum)}, ${year} at ${time}`;
+      }
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [reactionSchema]
+  },
   {
     toJSON: {
       getters: true,
     },
   }
 );
+
+thoughtSchema.virtual('reactionCount', () => this.reactions.length);
 
 const Thought = model('thought', thoughtSchema);
 
--- models/User.js
+++ models/User.js
@@ -14,10 +14,10 @@ const userSchema = new Schema(
       unique: true,
       match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
     },
-    thoughts: {
+    thoughts: [{
       type: Types.ObjectId,
       ref: 'thought'
-    },
+    }],
     friends: {
       type: Types.ObjectId,
       ref: 'user',
