a3d6fb4e99129aa66cbf15fdd144be78b53ca17d
KingdomSeeker328 <30511892+KingdomSeeker328@users.noreply.github.com>
2023-01-14 13:24:18 -0500

    Created the User schema

--- models/User.js
+++ models/User.js
@@ -1,14 +1,40 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 
 const userSchema = new Schema(
-  {},
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/
+    },
+    thoughts: {
+      type: Types.ObjectId,
+      ref: 'thought'
+    },
+    friends: {
+      type: Types.ObjectId,
+      ref: 'user',
+      validate: {
+        validator: (id) => id !== this._id,
+        message: 'You cannot befriend yourself!'
+      }
+    }
+  },
   {
     toJSON: {
-      virtuals: true,
-    },
-    id: false,
+      virtuals: true
+    }
   }
 );
+
+userSchema.virtual('friendCount').get(() => this.friends.length);
 
 const User = model('user', userSchema);
 
